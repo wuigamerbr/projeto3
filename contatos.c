7841974fbafbdb74169bd81b9e4be8543aa6a3dc
RafaelVS <160777850+RafaelANTVS@users.noreply.github.com>
2024-04-26 00:11:18 +0000

    verificando se o codigo está sem conflitos

--- contatos.c
+++ contatos.c
@@ -1,68 +1,58 @@
 #include <stdio.h>
 #include <string.h>
 #include "contatos.h"
-#include <ctype.h>
 
-ERROS criar(Agenda contatos[], int *pos) {
-    if (*pos >= TOTAL)
+ERROS criar(Agenda contatos[], int *pos){
+    if(*pos >= TOTAL)
         return MAX_AGENDA;
 
     printf("Entre com o nome do contato: ");
-    scanf("%99s", contatos[*pos].nome);
+    scanf("%s", &contatos[*pos].nome);
     clearBuffer();
 
     printf("Entre com o email: ");
-    scanf("%99s", contatos[*pos].email);
-    clearBuffer();
+    scanf("%s", &contatos[*pos].email);
 
     printf("Entre com o telefone: ");
-    scanf("%19s", contatos[*pos].telefone);
-    clearBuffer();
+    scanf("%d", &contatos[*pos].telefone);
 
-    (*pos)++;
-
-    return OK;
-}
-
-ERROS listar(Agenda contatos[], int *pos) {
-    if (*pos == 0)
-        return SEM_CONTATOS;
-
-    for (int i = 0; i < *pos; i++) {
-        printAgenda(contatos[i], i + 1);
-    }
+    *pos = *pos + 1;
 
     return OK;
 }
 
+ERROS deletar(Agenda contatos[], int *pos){
+    if(*pos == 0)
 
-
-ERROS deletar(Agenda contatos[], char *telefone, int *pos) {
-    int i;
-    int encontrado = 0;
-
-    if (*pos <= 0) {
         return SEM_CONTATOS;
-    }
 
-    for (i = 0; i < *pos; i++) {
-        if (strcmp(contatos[i].telefone, telefone) == 0) {
-            encontrado = 1;
-
-            for (int j = i; j < *pos - 1; j++) {
-                strcpy(contatos[j].nome, contatos[j + 1].nome);
-                strcpy(contatos[j].email, contatos[j + 1].email);
-                strcpy(contatos[j].telefone, contatos[j + 1].telefone);
-            }
-            (*pos)--;
-            printf("Contato removido com sucesso!\n");
-            break;
-        }
-    }
-
-    if (!encontrado) {
-        printf("Contato com o número de telefone %s não encontrado.\n", telefone);
+    int pos_deletar;
+    printf("Entre com o numero do contato que deseja deletar: ");
+    scanf("%d", &pos_deletar);
+    pos_deletar--;
+    if(pos_deletar >= *pos || pos_deletar < 0)
         return NAO_ENCONTRADO;
+
+    for(int i = pos_deletar; i < *pos; i++){
+        contatos[i].telefone = contatos[i+1].telefone;
+        strcpy(contatos[i].nome, contatos[i+1].nome);
+        strcpy(contatos[i].email,  contatos[i+1].email);
+    }
+
+    *pos = *pos - 1;
+
+    return OK;
+}
+
+ERROS listar(Agenda contatos[], int *pos){
+    if(*pos == 0)
+        return SEM_CONTATOS;
+
+    for(int i=0; i<*pos; i++){
+        printf("Pos: %c\t", i+1);
+        printf("Nome: %c\t", contatos[i].nome);
+        printf("email: %d\t", contatos[i].email);
+        printf("telefone: %d\n", contatos[i].telefone);
     }
 
     return OK;
@@ -91,34 +81,26 @@ ERROS salvar(Agenda contatos[], int *pos, int tamanho) {
     if (fclose(f) != 0)
         return FECHAR;
 
-    return OK;
-}
-
-ERROS carregar(Agenda contatos[], int *pos, int tamanho) {
-    FILE *f = fopen("agenda.bin", "rb");
-    if (f == NULL)
-        return ABRIR;
-
-    int i = fread(contatos, sizeof(Agenda), tamanho, f);
-    if (i != *pos)
-        return LER;
-
-    if (fread(pos, sizeof(int), 1, f) != 1)
-        return LER;
-
-    if (fclose(f) != 0)
-        return FECHAR;
-
     return OK;
 }
 
-void printAgenda(Agenda contatos, int pos) {
-    printf("\nPosicao: %d\t", pos);
-    printf("Nome: %s\tEmail: %s\t", contatos.nome, contatos.email);
-    printf("Telefone: %s\n", contatos.telefone);
-}
+ERROS carregar(Agenda contatos[], int *pos, int tamanho){
+  FILE *f = fopen("agenda.bin", "rb");
+  if (f == NULL)
+    return ABRIR;
 
-void clearBuffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+  int i = fread(contatos, tamanho, sizeof(Agenda), f);
+  if(i <= 0)
+    return LER;
+
+  i = fread(pos, 1, sizeof(int), f);
+  if(i <= 0)
+    return LER;
+  
+  i = fclose(f);
+  if(i == 0)
+    return FECHAR;
+  
+  return OK;
+
 }
