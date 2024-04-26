d8919bdf3089eef4f33e1d1f408034cdaae378f7
wui <114761764+wuigamerbr@users.noreply.github.com>
2024-04-26 00:38:42 +0000

    função deletar incrementada

--- contatos.c
+++ contatos.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include "contatos.h"
-#include <ctype.h>
 
 ERROS criar(Agenda contatos[], int *pos) {
     if (*pos >= TOTAL)
-        return MAX_AGENDA;
+        printf("Agenda lotada!");
 
     printf("Entre com o nome do contato: ");
     scanf("%99s", contatos[*pos].nome);
@@ -26,7 +25,7 @@ ERROS criar(Agenda contatos[], int *pos) {
 
 ERROS listar(Agenda contatos[], int *pos) {
     if (*pos == 0)
-        return SEM_CONTATOS;
+        printf("Sem contatos para exibir!");
 
     for (int i = 0; i < *pos; i++) {
         printAgenda(contatos[i], i + 1);
@@ -72,38 +71,38 @@ ERROS salvar(Agenda contatos[], int *pos, int tamanho) {
     FILE *f = fopen("agenda.bin", "wb");
     if (f == NULL)
         return ABRIR;
-
-    int qtd = fwrite(contatos, sizeof(Agenda), *pos, f);
-    if (qtd != *pos)
+    for (int i = 0; i < *pos; i++) {
+        if (fwrite(&contatos[i], sizeof(Agenda), 1, f) != 1) {
+            fclose(f);
+            return ESCREVER;
+        }
+    }
+    if (fwrite(pos, sizeof(int), 1, f) != 1) {
+        fclose(f);
         return ESCREVER;
-
-    if (fwrite(pos, sizeof(int), 1, f) != 1)
-        return ESCREVER;
-
+    }
     if (fclose(f) != 0)
         return FECHAR;
-
+        printf("Arquivo salvo com sucesso!");
     return OK;
 }
 
 ERROS carregar(Agenda contatos[], int *pos, int tamanho) {
     FILE *f = fopen("agenda.bin", "rb");
-    if (f == NULL)
+    if (f == NULL) {
+        printf("Erro ao abrir o arquivo agenda.bin\n");
         return ABRIR;
-
-    int i = fread(contatos, sizeof(Agenda), tamanho, f);
-    if (i != *pos)
-        return LER;
-
-    if (fread(pos, sizeof(int), 1, f) != 1)
-        return LER;
-
-    if (fclose(f) != 0)
+    }
+    int i;
+    for (i = 0; i < tamanho && fread(&contatos[i], sizeof(Agenda), 1, f) == 1; i++);
+    *pos = i;
+    if (fclose(f) != 0) {
+        printf("Erro ao fechar o arquivo agenda.bin\n");
         return FECHAR;
-
+    }
+    printf("Arquivo aberto com sucesso!");
     return OK;
 }
-
 void printAgenda(Agenda contatos, int pos) {
     printf("\nPosicao: %d\t", pos);
     printf("Nome: %s\tEmail: %s\t", contatos.nome, contatos.email);
